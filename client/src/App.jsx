b763b55ee358aa8375878d7b6b2a9d1d946f5f16
Hemant7777777 <hemantraju7777777@gmail.com>
2024-12-24 21:33:07 +0530

    app routes updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,9 +7,9 @@ import Profile from './pages/Profile';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './pages/CreateListing';
-//import UpdateListing from './pages/UpdateListing';
+import UpdateListing from './pages/UpdateListing';
 import Listing from './pages/Listing';
-//import Search from './pages/Search';
+import Search from './pages/Search';
 
 export default function App() {
   return (
@@ -20,16 +20,16 @@ export default function App() {
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
         <Route path='/about' element={<About />} />
-        {/* <Route path='/search' element={<Search />} /> */}
+        <Route path='/search' element={<Search />} />
         <Route path='/listing/:listingId' element={<Listing />} />
 
         <Route element={<PrivateRoute />}>
           <Route path='/profile' element={<Profile />} />
           <Route path='/create-listing' element={<CreateListing />} />
-          {/* <Route
+          <Route
             path='/update-listing/:listingId'
             element={<UpdateListing />}
-          /> */}
+          />
         </Route>
       </Routes>
     </BrowserRouter>
